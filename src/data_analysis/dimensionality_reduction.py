c85dbe188b1bd11296273e7e5f3b1ae354e17dbb
Sanghyun Ko <sang.hyun.ko@cern.ch>
2019-06-10 01:01:10 +0900

    test with shallow layers

--- src/data_analysis/dimensionality_reduction.py
+++ src/data_analysis/dimensionality_reduction.py
@@ -3,83 +3,55 @@ import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
 from numpy.random import seed
-from sklearn.preprocessing import minmax_scale
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.layers import Input, Dense
 from tensorflow.keras.models import Model
+from tensorflow.keras import optimizers
 
 def reduce(df_items, splitting_point, encoding_dim, nmbs_epoch):
 
+    df_items = df_items.drop('reference',axis=1)
     df_train = np.split(df_items, [splitting_point])[0]
-    df_train.drop(['reference'], axis=1, inplace=True)
-    df_items.drop(['reference'], axis=1, inplace=True)
 
-    print('Train data shape', df_train.shape)
-    print('Item data shape', df_items.shape)
-    
-    #as we only have 1,0 values we actually dont need to scale I think
-    train_scaled = minmax_scale(df_train, axis = 0)
-    item_scaled = minmax_scale(df_items, axis = 0)
-
-    ncol = train_scaled.shape[1]
+    ncol = df_train.shape[1]
 
     #choose random 80% for training
-    mask = np.random.choice(2,train_scaled.shape[0], p=[0.2,0.8])
-    X_train = train_scaled[mask == 1]
-    print('Xtrain data shape', X_train.shape)
-    X_test = train_scaled[mask == 0]
-    print('Xtest data shape', X_test.shape)
+    mask = np.random.choice(2,df_train.shape[0], p=[0.2,0.8])
+    X_train = df_train[mask == 1]
+    X_test = df_train[mask == 0]
 
     input_dim = Input(shape = (ncol, ))
 
     # Encoder Layers
-    encoded1 = Dense(3000, activation = 'relu')(input_dim)
-    encoded2 = Dense(2750, activation = 'relu')(encoded1)
-    encoded3 = Dense(2500, activation = 'relu')(encoded2)
-    encoded4 = Dense(2250, activation = 'relu')(encoded3)
-    encoded5 = Dense(2000, activation = 'relu')(encoded4)
-    encoded6 = Dense(1750, activation = 'relu')(encoded5)
-    encoded7 = Dense(1500, activation = 'relu')(encoded6)
-    encoded8 = Dense(1250, activation = 'relu')(encoded7)
-    encoded9 = Dense(1000, activation = 'relu')(encoded8)
-    encoded10 = Dense(750, activation = 'relu')(encoded9)
-    encoded11 = Dense(500, activation = 'relu')(encoded10)
-    encoded12 = Dense(250, activation = 'relu')(encoded11)
-    encoded13 = Dense(encoding_dim, activation = 'relu')(encoded12)
+    encoded1 = Dense(100, activation='relu')(input_dim)
+    encoded2 = Dense(50, activation = 'relu')(encoded1)
+    encoded3 = Dense(encoding_dim, activation = 'relu')(encoded2)
 
     # Decoder Layers
-    decoded1 = Dense(250, activation = 'relu')(encoded13)
-    decoded2 = Dense(500, activation = 'relu')(decoded1)
-    decoded3 = Dense(750, activation = 'relu')(decoded2)
-    decoded4 = Dense(1000, activation = 'relu')(decoded3)
-    decoded5 = Dense(1250, activation = 'relu')(decoded4)
-    decoded6 = Dense(1500, activation = 'relu')(decoded5)
-    decoded7 = Dense(1750, activation = 'relu')(decoded6)
-    decoded8 = Dense(2000, activation = 'relu')(decoded7)
-    decoded9 = Dense(2250, activation = 'relu')(decoded8)
-    decoded10 = Dense(2500, activation = 'relu')(decoded9)
-    decoded11 = Dense(2750, activation = 'relu')(decoded10)
-    decoded12 = Dense(3000, activation = 'relu')(decoded11)
-    decoded13 = Dense(ncol, activation = 'sigmoid')(decoded12)
+    decoded1 = Dense(50, activation = 'relu')(encoded3)
+    decoded2 = Dense(100, activation = 'relu')(decoded1)
+    decoded3 = Dense(ncol, activation = 'sigmoid')(decoded2)
 
     # Combine Encoder and Decoder layers
-    autoencoder = Model(inputs = input_dim, outputs = decoded13)
+    autoencoder = Model(inputs = input_dim, outputs = decoded3)
 
     # Compile the Model
-    autoencoder.compile(optimizer = 'adadelta', loss = 'binary_crossentropy')
+    opt = optimizers.Adagrad(lr=0.1)
+    autoencoder.compile(optimizer = opt, loss = 'binary_crossentropy')
 
     autoencoder.summary()
 
-    autoencoder.fit(X_train, X_train, nb_epoch = nmbs_epoch, batch_size = 32, 
-                    shuffle = False, validation_data = (X_test, X_test))    
-    print("training completed")
+    autoencoder.fit(X_train, X_train, nb_epoch = nmbs_epoch, batch_size = 32,
+                    shuffle = False, validation_data = (X_test, X_test))
+    print("Training completed")
 
     #Use Encoder level to reduce dimension of train and test data
-    encoder = Model(inputs = input_dim, outputs = encoded13)
+    encoder = Model(inputs = input_dim, outputs = encoded3)
     encoded_input = Input(shape = (encoding_dim, ))
-    print(f"encoding item features to {encoding_dim} dimensions... (this takes a while)")
+
+    print(f"Encoding item features to {encoding_dim} dimensions... (this takes a while)")
     #Predict the new train and test data using Encoder
-    encoded_item = pd.DataFrame(encoder.predict(item_scaled))
+    encoded_item = pd.DataFrame(encoder.predict(df_items))
     encoded_item = encoded_item.add_prefix('feature_')
 
     return encoded_item
--- src/data_analysis/dimred_main.py
+++ src/data_analysis/dimred_main.py
@@ -22,46 +22,45 @@ def main(data_path):
 
     meta_encoded_csv = data_directory.joinpath('item_metadata_encoded.csv')
     dimred_encoded_item_csv = data_directory.joinpath('dimred_encoded_item.csv')
-    print(dimred_encoded_item_csv)
 
     print(f"Reading {meta_encoded_csv}...")
     df_items = pd.read_csv(meta_encoded_csv)
 
-    all_keys = ['1 Star', '2 Star', '3 Star', '4 Star', '5 Star', 
-                'Accessible Hotel', 'Accessible Parking', 'Adults Only', 'Air Conditioning', 'Airport Hotel', 
-                'Airport Shuttle', 'All Inclusive (Upon Inquiry)', 'Balcony', 'Bathtub', 'Beach', 'Beach Bar', 
-                'Beauty Salon', 'Bed & Breakfast', 'Bike Rental', 'Boat Rental', 'Body Treatments', 
-                'Boutique Hotel', 'Bowling', 'Bungalows', 'Business Centre', 'Business Hotel', 'Cable TV', 
-                'Camping Site', 'Car Park', 'Casa Rural (ES)', 'Casino (Hotel)', 'Central Heating', 'Childcare', 
-                'Club Hotel', 'Computer with Internet', 'Concierge', 'Conference Rooms', 'Convenience Store', 
-                'Convention Hotel', 'Cosmetic Mirror', 'Cot', 'Country Hotel', 'Deck Chairs', 'Design Hotel', 
-                'Desk', 'Direct beach access', 'Diving', 'Doctor On-Site', 'Eco-Friendly hotel', 
-                'Electric Kettle', 'Excellent Rating', 'Express Check-In / Check-Out', 'Family Friendly', 'Fan', 
-                'Farmstay', 'Fitness', 'Flatscreen TV', 'Free WiFi (Combined)', 'Free WiFi (Public Areas)', 
-                'Free WiFi (Rooms)', 'Fridge', 'From 2 Stars', 'From 3 Stars', 'From 4 Stars', 'Gay-friendly', 
-                'Golf Course', 'Good Rating', 'Guest House', 'Gym', 'Hairdresser', 'Hairdryer', 'Halal Food', 
-                'Hammam', 'Health Retreat', 'Hiking Trail', 'Honeymoon', 'Horse Riding', 'Hostal (ES)', 'Hostel', 
-                'Hot Stone Massage', 'Hotel', 'Hotel Bar', 'House / Apartment', 'Hydrotherapy', 'Hypoallergenic Bedding', 
-                'Hypoallergenic Rooms', 'Ironing Board', 'Jacuzzi (Hotel)', "Kids' Club", 'Kosher Food', 'Large Groups', 
-                'Laundry Service', 'Lift', 'Luxury Hotel', 'Massage', 'Microwave', 'Minigolf', 'Motel', 'Nightclub', 
-                'Non-Smoking Rooms', 'On-Site Boutique Shopping', 'Openable Windows', 'Organised Activities', 
-                'Pet Friendly', 'Playground', 'Pool Table', 'Porter', 'Pousada (BR)', 'Radio', 'Reception (24/7)', 
-                'Resort', 'Restaurant', 'Romantic', 'Room Service', 'Room Service (24/7)', 'Safe (Hotel)', 'Safe (Rooms)', 
-                'Sailing', 'Satellite TV', 'Satisfactory Rating', 'Sauna', 'Self Catering', 'Senior Travellers', 
-                'Serviced Apartment', 'Shooting Sports', 'Shower', 'Singles', 'Sitting Area (Rooms)', 'Ski Resort', 'Skiing', 
-                'Solarium', 'Spa (Wellness Facility)', 'Spa Hotel', 'Steam Room', 'Sun Umbrellas', 'Surfing', 
-                'Swimming Pool (Bar)', 'Swimming Pool (Combined Filter)', 'Swimming Pool (Indoor)', 'Swimming Pool (Outdoor)', 
-                'Szep Kartya', 'Table Tennis', 'Telephone', 'Teleprinter', 'Television', 'Tennis Court', 
-                'Tennis Court (Indoor)', 'Terrace (Hotel)', 'Theme Hotel', 'Towels', 'Very Good Rating', 'Volleyball', 
+    all_keys = ['1 Star', '2 Star', '3 Star', '4 Star', '5 Star',
+                'Accessible Hotel', 'Accessible Parking', 'Adults Only', 'Air Conditioning', 'Airport Hotel',
+                'Airport Shuttle', 'All Inclusive (Upon Inquiry)', 'Balcony', 'Bathtub', 'Beach', 'Beach Bar',
+                'Beauty Salon', 'Bed & Breakfast', 'Bike Rental', 'Boat Rental', 'Body Treatments',
+                'Boutique Hotel', 'Bowling', 'Bungalows', 'Business Centre', 'Business Hotel', 'Cable TV',
+                'Camping Site', 'Car Park', 'Casa Rural (ES)', 'Casino (Hotel)', 'Central Heating', 'Childcare',
+                'Club Hotel', 'Computer with Internet', 'Concierge', 'Conference Rooms', 'Convenience Store',
+                'Convention Hotel', 'Cosmetic Mirror', 'Cot', 'Country Hotel', 'Deck Chairs', 'Design Hotel',
+                'Desk', 'Direct beach access', 'Diving', 'Doctor On-Site', 'Eco-Friendly hotel',
+                'Electric Kettle', 'Excellent Rating', 'Express Check-In / Check-Out', 'Family Friendly', 'Fan',
+                'Farmstay', 'Fitness', 'Flatscreen TV', 'Free WiFi (Combined)', 'Free WiFi (Public Areas)',
+                'Free WiFi (Rooms)', 'Fridge', 'From 2 Stars', 'From 3 Stars', 'From 4 Stars', 'Gay-friendly',
+                'Golf Course', 'Good Rating', 'Guest House', 'Gym', 'Hairdresser', 'Hairdryer', 'Halal Food',
+                'Hammam', 'Health Retreat', 'Hiking Trail', 'Honeymoon', 'Horse Riding', 'Hostal (ES)', 'Hostel',
+                'Hot Stone Massage', 'Hotel', 'Hotel Bar', 'House / Apartment', 'Hydrotherapy', 'Hypoallergenic Bedding',
+                'Hypoallergenic Rooms', 'Ironing Board', 'Jacuzzi (Hotel)', "Kids' Club", 'Kosher Food', 'Large Groups',
+                'Laundry Service', 'Lift', 'Luxury Hotel', 'Massage', 'Microwave', 'Minigolf', 'Motel', 'Nightclub',
+                'Non-Smoking Rooms', 'On-Site Boutique Shopping', 'Openable Windows', 'Organised Activities',
+                'Pet Friendly', 'Playground', 'Pool Table', 'Porter', 'Pousada (BR)', 'Radio', 'Reception (24/7)',
+                'Resort', 'Restaurant', 'Romantic', 'Room Service', 'Room Service (24/7)', 'Safe (Hotel)', 'Safe (Rooms)',
+                'Sailing', 'Satellite TV', 'Satisfactory Rating', 'Sauna', 'Self Catering', 'Senior Travellers',
+                'Serviced Apartment', 'Shooting Sports', 'Shower', 'Singles', 'Sitting Area (Rooms)', 'Ski Resort', 'Skiing',
+                'Solarium', 'Spa (Wellness Facility)', 'Spa Hotel', 'Steam Room', 'Sun Umbrellas', 'Surfing',
+                'Swimming Pool (Bar)', 'Swimming Pool (Combined Filter)', 'Swimming Pool (Indoor)', 'Swimming Pool (Outdoor)',
+                'Szep Kartya', 'Table Tennis', 'Telephone', 'Teleprinter', 'Television', 'Tennis Court',
+                'Tennis Court (Indoor)', 'Terrace (Hotel)', 'Theme Hotel', 'Towels', 'Very Good Rating', 'Volleyball',
                 'Washing Machine', 'Water Slide', 'Wheelchair Accessible', 'WiFi (Public Areas)', 'WiFi (Rooms)']
-    
+
     #keys connected to rating
     rating_keys = ['1 Star', '2 Star', '3 Star', '4 Star', '5 Star', 'From 2 Stars', 'From 3 Stars', 'From 4 Stars']
     no_rating_keys = [key for key in all_keys if key not in rating_keys]
-    
+
     #define keys, that are a subjective rating like "4 Stars" or "Romantic Hotel"
-    subjective_keys = ['1 Star', '2 Star', '3 Star', '4 Star', '5 Star', 'From 2 Stars', 'From 3 Stars', 'From 4 Stars', 
-                       'Excellent Rating', 'Gay-friendly', 'Honeymoon', 'Satisfactory Rating', 'Senior Travellers', 
+    subjective_keys = ['1 Star', '2 Star', '3 Star', '4 Star', '5 Star', 'From 2 Stars', 'From 3 Stars', 'From 4 Stars',
+                       'Excellent Rating', 'Gay-friendly', 'Honeymoon', 'Satisfactory Rating', 'Senior Travellers',
                        'Very Good Rating']
 
     objective_keys = [key for key in all_keys if key not in subjective_keys]
@@ -70,19 +69,19 @@ def main(data_path):
     #TODO: send different df_items: (No stars, no subject, etc...)
     #TODO: different encoding dimensions
     #TODO: different number of epochs
-    #TODO: maybe try larger training sample than 4000, but looking at online samples they 
+    #TODO: maybe try larger training sample than 4000, but looking at online samples they
     #      don't use more than around 4000 for training of encoder
     #FUNCTION: dimred.reduce(dataframe, splitting point, encod_dim, nb_epoch)
 
     #Option 1: small test to see if everything is working (not full itemset)
-    encoded_item = dimred.reduce(df_items[0:10000], 1000, 20, 1)
-    print(f"Writing to {dimred_encoded_item_csv} ...")
-    encoded_item.to_csv(dimred_encoded_item_csv, index=False)
-
-    # #Option 2: one full run with one set of reasonable parameters
-    # encoded_item = dimred.reduce(df_items, 4000, 20, 200)
+    # encoded_item = dimred.reduce(df_items[0:10000], 1000, 20, 1)
     # print(f"Writing to {dimred_encoded_item_csv} ...")
     # encoded_item.to_csv(dimred_encoded_item_csv, index=False)
+
+    # #Option 2: one full run with one set of reasonable parameters
+    encoded_item = dimred.reduce(df_items, 10000, 20, 100)
+    print(f"Writing to {dimred_encoded_item_csv} ...")
+    encoded_item.to_csv(dimred_encoded_item_csv, index=False)
 
     # #Option 3: LONG: create a set of useful datasets for analysis with kmeans
     # counter = 0
